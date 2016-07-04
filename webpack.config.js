5e526db3af0d1ba1cdbdfec8af1a537b3f29a38a
wangwenfang <wangwenfang.didichuxing.com>
2016-07-04 10:09:20 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules').filter(function(x) {
+	return ['.bin'].indexOf(x) === - 1;
+}).forEach(function(mod) {
+	nodeModules[mod] = 'commonjs ' + mod;
+});
+
+module.exports = {
+	entry: ['babel-polyfill', path.resolve(__dirname, './src/index.js')],
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'server.js'
+	},
+	target: "node",
+	externals: nodeModules,
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			loader: 'babel-loader',
+		}]
+	}
+}
+
